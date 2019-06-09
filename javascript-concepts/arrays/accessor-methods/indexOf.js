da9d1cd1a333886697c40e40c4eb3883020f375f
CodeAgony <yaroslav.bkh@gmail.com>
2019-06-09 15:56:57 +0300

    Add indexOf

---
+++ javascript-concepts/arrays/accessor-methods/indexOf.js
@@ -0,0 +1,11 @@
+// The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
+
+let flowers = ["Dandellion", "Rose", "Sunflower", "Orchid", "Tulip"]
+
+let indexOfRose = flowers.indexOf("Rose");
+
+console.log(indexOfRose); 
+// Expected return - 1
+
+console.log(flowers.indexOf("Rse"))
+// Expected return - -1
